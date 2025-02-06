c70f839150b87def70aa75e0606280d08a5738f0
Kowshik Venigalla <kowshikeswarachaitanya@gmail.com>
2025-02-06 20:40:52 +0530

    Initial commit with frontend and backend

---
+++ auth-app-springboot-backend/src/main/java/com/kowshik/auth_app_springboot_backend/AuthAppSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kowshik.auth_app_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ auth-app-springboot-backend/src/test/java/com/kowshik/auth_app_springboot_backend/AuthAppSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kowshik.auth_app_springboot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
